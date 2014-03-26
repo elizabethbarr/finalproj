d8a8d3eb2e350e350cbb2a6bf86141e2f744182f
Elizabeth Barr <elizbarr@gmail.com>
2014-03-26 12:15:51 -0400

    testy test test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
 console.log('\'Allo \'Allo!');
+
+Parse.initialize("gnlEkmksPP0cjEXhLSHT0U6nUrjk1os4Hm1RDTs7", "JGA4hNgJx57nJ5x36XcECP7GIRXYKZecdd9iscjI");
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});
